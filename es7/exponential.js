24f2b87fff09e318159037efd58075c2b6e4139f
juanjosechiroque <juanjosechiroque@gmail.com>
2021-01-28 19:42:40 -0500

    add es7 features

---
+++ es7/exponential.js
@@ -0,0 +1,6 @@
+const base = 4;
+const exponent = 3;
+
+const result = base ** exponent;
+
+console.log(result);
---
+++ es7/includes.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4, 5];
+
+console.log('existe el numero 5: ' + numbers.includes(5));
+console.log('existe el numero 7: ' + numbers.includes(7));
